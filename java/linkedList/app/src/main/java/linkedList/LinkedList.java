4610e6ad7f957593771c61f56ce94d11fced977d
mohammadkabbara <mohammadkabbara40@gmail.com>
2021-10-04 01:57:17 +0300

    solved

--- java/linkedList/app/src/main/java/linkedList/LinkedList.java
+++ java/linkedList/app/src/main/java/linkedList/LinkedList.java
@@ -8,48 +8,50 @@ public class LinkedList {
 
   /////////insert//////////
 
-  //  public void insert(int data) {
-//    Node newNode = new Node(data);
-//    if (head == null) {
-//      head = newNode;
-//      tail=head;
-//    }else {
-//      tail.next = newNode;
-//      tail=newNode;
-//    }
-//
-//  }
-//  /////////include//////////
-//public boolean include(int data){
-//  Node currentNode =head;
-//  while(currentNode!=null){
-//    if(data ==currentNode.data){
-//      System.out.println("it is include ");
-//      return true;
-//    }
-//    currentNode=currentNode.next;
-//  }
-//  System.out.println("it is not include");
-//  return false;
-//}
-//  /////////toString//////////
-//
-//  public String toString(){
-//    String toStringList="";
-//    Node currentNode = head;
-//    if (head==null){
-//      System.out.println("the list empty");
-//
-//    }
-//    while (currentNode!=null){
-//      toStringList += "{"  + currentNode.data +"} ==>";
-//      System.out.print("{" + currentNode.data + "}" + "==> ");
-//      currentNode = currentNode.next;
-//    }
-//    System.out.println("null");
-//    return toStringList;
-//  }
-  //////////////////////////////////////////////////////// code challenge 6///////////////////////////////////////////
+    public void insert(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = newNode;
+      tail=head;
+    }else {
+      tail.next = newNode;
+      tail=newNode;
+    }
+
+  }
+  /////////include//////////
+public boolean include(int data){
+  Node currentNode =head;
+  while(currentNode!=null){
+    if(data ==currentNode.data){
+      System.out.println("it is include ");
+      return true;
+    }
+    currentNode=currentNode.next;
+  }
+  System.out.println("it is not include");
+  return false;
+}
+  /////////toString//////////
+
+  public String toString(){
+    String toStringList="";
+    Node currentNode = head;
+    if (head==null){
+      System.out.println("the list empty");
+
+    }
+    while (currentNode!=null){
+      toStringList += "{"  + currentNode.data +"} ==>";
+      System.out.print("{" + currentNode.data + "}" + "==> ");
+      currentNode = currentNode.next;
+    }
+    System.out.println("null");
+    return toStringList;
+  }
+
+
+  ////////////////////////// code challenge 6////////////////////
   public void append(int data){
     Node append = new Node(data);
     if (head == null) {
@@ -84,6 +86,33 @@ public class LinkedList {
       }
     }
   }
+
+
+  public void insertAfter(int value , int newValue ){
+    if (head != null) {
+      if (value == tail.data){
+      Node newNode = new Node (newValue);
+      tail.next = newNode;
+      tail=newNode;
+      }else {
+
+        Node current = head ;
+        Node next =current.next;
+        while (current!=null){
+          if (value==current.data){
+            Node newNode = new Node(newValue);
+            newNode.next=next;
+            current.next=newNode;
+            break;
+          }
+          if (current.next!=null){
+            current=current.next;
+            next=current.next;
+          }else {
+            current=current.next;
+          }
+        }
+      }
+    }
+  }
 }
-
-
