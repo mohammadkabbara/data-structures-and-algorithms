cdae30f0e2fe50869404440483f5867009ec1865
mohammadkabbara <mohammadkabbara40@gmail.com>
2021-10-07 23:23:58 +0300

    solved

---
+++ java/stackAndQueue/lib/src/main/java/stackAndQueue/Node.java
@@ -0,0 +1,11 @@
+package stackAndQueue;
+
+public class Node<T> {
+
+  public T value;
+  public Node next;
+
+  public Node(T value){
+    this.value = value;
+  }
+}
---
+++ java/stackAndQueue/lib/src/main/java/stackAndQueue/Stack.java
@@ -0,0 +1,53 @@
+package stackAndQueue;
+
+public class Stack<T> {
+
+  Node<T> top;
+
+  public void push(T value){
+    Node<T> newNode = new Node<T>(value);
+    newNode.next = top;
+    top = newNode;
+  }
+
+  public T pop() {
+    try {
+      Node<T> temp = top;
+      top = top.next;
+      temp.next = null;
+      return temp.value;
+    } catch (NullPointerException e) {
+      e.printStackTrace();
+      System.out.println("it is empty");
+      return null;
+    }
+  }
+
+  public T peek() {
+    try {
+      return top.value;
+    } catch (NullPointerException e) {
+      e.printStackTrace();
+      System.out.println("it is empty");
+      return null;
+    }
+  }
+
+  public boolean isEmpty(){
+    if (top == null) return true;
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    if (top == null) return "null";
+    String outList = "Top = ";
+    Node<T> current = top;
+    while (current != null){
+      outList += current.value+ " = > ";
+      current = current.next;
+    }
+    outList += "Null";
+    return outList;
+  }
+}
